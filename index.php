5ea792ed9cf85ac7d93e11ccb31444428fbe5b8b
wfredo-dev <wfredo.dev@gmail.com>
2025-01-26 14:45:46 +0800

    create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Project</h1>
+    <p>My first php project on github </p>
+    <p>this will become my upcoming portfolio website</p>
+</body>
+</html>
