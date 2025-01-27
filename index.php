68d360dca349ca81b04bd3ab67c54fa9e6f29b00
wfredo-dev <wfredo.dev@gmail.com>
2025-01-27 09:06:09 +0800

    porftolio layout

--- index.php
+++ index.php
@@ -5,66 +5,110 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        html,
-body {
-  height: 100%; /* Ensures the parent elements cover full height */
-  margin: 0;
+        *{
+            scroll-behavior: smooth;
+            box-sizing: border-box;
+            text-decoration: none;
+            border: none;
+            outline: none;  
+            font-family: 'Courier New', Courier, monospace;
+        }
+
+        html,body {
+            height: 100%; /* Ensures the parent elements cover full height */
+            margin: 0;
+            color:var(--text-color);
+            background-color: var(--bg-color);
   
-}
-    
-        .container{
-            display: grid;
-            gap: 5px;
-            grid-template-areas: "header header"
-                                    "nav main"
-                                    "footer footer";
-        
-         grid-template-columns:300px auto;
+        }
+        header{
+
+        }
+        section{
+        display: block;
+        unicode-bidi: isolate;
+        }
+        :root{
+        --bg-color:#fff;
+        --snd-bg-color:#d5fdf9;
+        --text-color:#000;
+        --main-color:#007f73;
+        }
+        .home{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        }
+        .aboutme{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--main-color);
+        }
+        .projects{
+        display: flex;
        
-            background-color: #2196F3;
-            padding: 5px;
-            gap: 5px;
+ 
         }
-        .container > div{
-  background-color: rgba(14, 12, 12, 0.8);
-  padding: 100px;
-}
-        .container > div.header{
-            grid-area:header;
+        .contacts{
+        background-color: var(--main-color);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+         }
+        section{
+        min-height: 100vh;
         }
-        .container > div.nav{
-            grid-area:nav;
-         
+        header{
+            width: 100%;
+            background-color: var(--main-color);
+            position: fixed;
+            right: 0;
+            top: 0;
+            z-index: 1000;
+            display:flex;
+            justify-content: space-between;
+            align-items: center;
 
         }
-        .container > div.main{
-          grid-area:main;
-          height: 100vh; /* Full viewport height */
-          
-
+        .header{
+            padding: 2rem 3%;
         }
-        .container > div.footer{
-             grid-area:footer;
+        .navbar a{
+            margin-left: 4rem;
+        
         }
-        .container > div {
-  background-color: rgba(255, 255, 255, 0.8);
-  padding: 10px;
-}
+ 
     </style>
 </head>
 <body>
  <div class="container">
-    <div class="header"><h1>Wilfredo Fernandez</h1></div>
-    <div class="nav">
-        <ul>
-            <li>Home</li>
-            <li>AboutMe</li>
-            <li>Projects</li>
-            <li>Contacts</li>
-        </ul>
-    </div>
-    <div class="main">Main</div>
-    <div class="footer">Footer</div>
+
+    <header class="header" id="header">
+        <a href="#" class="logo" id="logo">Logo</a>
+        <nav class="navbar" id="navbar">
+
+<a href="#home">Home</a>
+<a href="#aboutme">Aboutme</a>
+<a href="#projects">Projects</a>
+<a href="#contacts">Contacts</a>
+
+</nav>
+    </header>
+
+    <section class="home" id="home">
+HOME
+    </section>
+    <section class="aboutme" id="aboutme">
+ABOUT ME
+    </section>
+    <section class="projects" id="projects">
+PROJECTs
+    </section>
+    <section class="contacts" id="contacts">
+CONTACTS
+    </section>
+
    
  </div>
 </body>
